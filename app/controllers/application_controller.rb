d90ffbd873246c1d46766a13b308fc4721728315
Jimmy@cp <jimmy@coolpur.com>
2010-01-04 10:45:50 +0800

    add login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,15 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # :secret => '8fc080370e56e929a2d5afca5540a0f7'
+  before_filter :authorize, :except => :login
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  protected 
+  def authorize
+    unless Member.find_by_id(session[:member_id])
+      flash[:notice] = "Please log in"
+      redirect_to :controller => 'member' , :action => 'login'
+    end
+  end
 end
